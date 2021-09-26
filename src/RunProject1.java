a39f4470a1c8977ff35a3a5683fd7f65f7305cf2
Heer Patel <83523957+heer-jp@users.noreply.github.com>
2021-09-26 17:06:48 -0400

    Add files via upload

--- src/RunProject1.java
+++ src/RunProject1.java
@@ -1,7 +1,14 @@
 package project1;
-
+/**
+ * Runs project1 by calling CollectionManager class run method. 
+ * @author Sai Maduri, Heer Patel
+ */
 public class RunProject1 {
-	
+	/**
+	 * Calls CollectionManager run method.
+	 * Reads all user input and 
+	 * @param args just checking to see if there is a javadoc 
+	 */
 	public static void main(String [] args) {
 	new CollectionManager().run(); 
 	}
