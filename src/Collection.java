66ac2b57eff817207e124925e538c0699b3ae88a
Heer Patel <83523957+heer-jp@users.noreply.github.com>
2021-09-26 18:47:16 -0400

    Update Collection.java

--- src/Collection.java
+++ src/Collection.java
@@ -1,121 +1,185 @@
 package project1;
-
+/**
+ * Collects and manages Album objects in albums array.
+ * Performs functions called by CollectionManager class.
+ * Functions include adding and removing Album objects from the array,
+ * lending out and returning Album objects - changing their availability status,
+ * and printing the array collection in no specified order, by album genres, and or by album release dates
+ * @author Sai Maduri, Heer Patel
+ */
 public class Collection {
-
-	private Album[] albums;
-	private int numAlbums;
-	
-	public Collection() {
-		this.albums = new Album[4];
-		this.numAlbums = 0;
-	}
-	
-	private int find(Album album) {
-		for (int i = 0; i < albums.length; i++) {
-			if (album.equals(albums[i])) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	public int getNumAlbums() {
-		return numAlbums;
-	}
-	
-	private void grow() {
-		Album[] oldAlbums = this.albums;
-		int newLength = oldAlbums.length + 4;
-		Album[] newAlbums = new Album[newLength];
-		for (int i = 0; i < oldAlbums.length; i++) {
-			newAlbums[i] = oldAlbums[i];
-		}
-		this.albums = newAlbums;
-	}
-	
-	public boolean add(Album album) { 
-		int index = 0;
-		for (int i = 0 ; i < albums.length; i++) {
-			if (albums[i] == null) {
-				index = i;
-			} else if(albums[i].equals(album)) {
-				return false;
-			}
-		}
-		albums[index] = album;
-		numAlbums++;
-		if (numAlbums == albums.length)
-			grow();
-		return true;
-	}
-	
-	public boolean remove(Album album) {
-		int index = find(album);
-		if (index < 0) 
-			return false;
-		albums[index] = null;
-		numAlbums--;
-		return true;
-	}
-	
-	public boolean lendingOut(Album album) {
-		int index = find(album);
-		if (!albums[index].getAvailability())
-			return false;
-		albums[index].setAvailability(false);
-		return true;
-	}
-	
-	public boolean returnAlbum(Album album) {
-		int index = find(album);
-		if (albums[index].getAvailability())
-			return false;
-		albums[index].setAvailability(true);
-		return true;
-	}
-	
-	public void print() {
-		System.out.println("*List of albums in the collection.");
-		for (Album album : albums) 
-			if (album != null)
-				System.out.println(album);
-		System.out.println("*End of list.");
-	}
-	
-	public void printByReleaseDate() {
-		
-	}
-	
-	public void printByGenre() {
-		
-		
-	}
-	
-	public static void main(String[] args) {
-		Collection collection = new Collection();
-		System.out.println(collection.add(new Album("Change", "Anika", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.add(new Album("Change", "Anika", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.add(new Album("Change", "April March", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.add(new Album("Spiral", "Darkside", Genre.Pop, new Date("7/1/2021"), true)));
-		System.out.println(collection.add(new Album("Debussy:La Mer", "Leonard Bernstein", Genre.Classical, new Date("2/28/1995"), true)));
-		System.out.println(collection.add(new Album("Debussy:La Mer", "Gary Bertini", Genre.Classical, new Date("1/1/2006"), true)));
-		System.out.println(collection.add(new Album("Debussy:La Mer", "Leonard Bernstein", Genre.Classical, new Date("2/28/1995"), true)));
-		collection.print();
-		System.out.print(collection.numAlbums);
-		System.out.println(collection.remove(new Album("Change", "April March")));
-		collection.print();
-		System.out.println(collection.add(new Album("Change", "April March", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.lendingOut(new Album("Change", "April March")));
-		System.out.println(collection.lendingOut(new Album("Change", "April March")));
-		System.out.println(collection.returnAlbum(new Album("Change", "April March")));
-		System.out.println(collection.returnAlbum(new Album("Change", "April March")));
-		System.out.println(collection.lendingOut(new Album("Change", "April March")));
-		System.out.println(collection.add(new Album("Change", "Anika", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.add(new Album("Change", "Anika", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.add(new Album("Change", "Anika", Genre.Pop, new Date("7/23/2021"), true)));
-		System.out.println(collection.remove(new Album("Chang", "Anika")));
-		collection.print();
-
-	}
+    private Album[] albums;
+    private int numAlbums;
+    /**
+     * Collection object array created.
+     */
+    public Collection() {
+        this.albums = new Album[4];
+        this.numAlbums = 0;
+    }
+    /**
+     * Finds if an album already exists in array collection.
+     * @param album object being searched for.
+     * @return index of where the album object is in the array.
+     */
+    private int find(Album album) {
+        for (int i = 0; i < albums.length; i++) {
+            if (album.equals(albums[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    /**
+     * Returns number of albums in album array.
+     * @return number of albums.
+     */
+    public int getNumAlbums() {
+        return numAlbums;
+    }
+    /**
+     * Grows array length if array is full.
+     */
+    private void grow() {
+        Album[] oldAlbums = this.albums;
+        int newLength = oldAlbums.length + 4;
+        Album[] newAlbums = new Album[newLength];
+        for (int i = 0; i < oldAlbums.length; i++) {
+            newAlbums[i] = oldAlbums[i];
+        }
+        this.albums = newAlbums;
+    }
+    /**
+     * Adds album object to albums array.
+     * @param album object to be added.
+     * @return false if album is not added, true if it is added.
+     */
+    public boolean add(Album album) {
+        for (int i = 0; i < albums.length; i++) {
+            if (albums[i] == null) {
+                for (int j = i + 1; j < albums.length; j++) {
+                    albums[j - 1] = albums[j];
+                }
+                albums[albums.length - 1] = null;
+            }
+        }
+        int albumIndex = 0;
+        for (int i = 0; i < albums.length; i++) {
+            if (albums[i] == null) {
+                albumIndex = i;
+            } else if (albums[i].equals(album)) {
+                return false;
+            }
+        }
+        albums[albumIndex] = album;
+        numAlbums++;
+        if (numAlbums == albums.length)
+            grow();
+        return true;
+    }
+    /**
+     * Removes album from array if it exists in the array. 
+     * @param album object to be deleted.
+     * @return false if album is not in array, true if it exists and is removed.
+     */
+    public boolean remove(Album album) {
+        int albumIndex = find(album);
+        if (albumIndex < 0)
+            return false;
+        albums[albumIndex] = null;
+        numAlbums--;
+        return true;
+    }
+    /**
+     * If album exists changes album availability to false.
+     * @param album object to be changed.
+     * @return true if changed false if not changed.
+     */
+    public boolean lendingOut(Album album) {
+        int albumIndex = find(album);
+        if (albumIndex < 0 || !albums[albumIndex].getAvailability())
+            return false;
+        albums[albumIndex].setAvailability(false);
+        return true;
+    }
+    /**
+     * If album exists changes album availability to true.
+     * @param album object to be changed.
+     * @return true if changed, false if not changed. 
+     */
+    public boolean returnAlbum(Album album) {
+        int albumIndex = find(album);
+        if (albumIndex < 0 || albums[albumIndex].getAvailability())
+            return false;
+        albums[albumIndex].setAvailability(true);
+        return true;
+    }
+    /** 
+     * Prints all albums in array, if array is not empty, in no specified order.
+     */
+    public void print() {
+        if (numAlbums != 0) {
+            System.out.println("*List of albums in the collection.");
+            for (Album album : albums)
+                if (album != null)
+                    System.out.println(album);
+            System.out.println("*End of list");
+        } else {
+            System.out.println("The collection is empty!");
+        }
+    }
+    /** 
+     * Prints all albums in array, if array is not empty, in order of release date.
+     */
+    public void printByReleaseDate() {
+        for (int i = 0; i < albums.length - 1; i++) {
+            int swapIndex = i;
+            for (int j = i + 1; j < albums.length; j++) {
+                if (albums[j] != null && albums[i] != null) {
+                    if (albums[j].getReleaseDate().compareTo(albums[swapIndex].getReleaseDate()) != -1) {
+                        swapIndex = j;
+                    }
+                }
+            }
+            Album tmpAlbum = albums[swapIndex];
+            albums[swapIndex] = albums[i];
+            albums[i] = tmpAlbum;
+        }
+        if (numAlbums != 0) {
+            System.out.println("*Album collection by the release dates.");
+            for (Album album : albums)
+                if (album != null)
+                    System.out.println(album);
+            System.out.println("*End of list");
+        } else {
+            System.out.println("The collection is empty!");
+        }
+    }
+    /** 
+     * Prints all albums in array, if array is not empty, in order of genre.
+     */
+    public void printByGenre() {
+        for (int i = 0; i < albums.length - 1; i++) {
+            int swapIndex = i;
+            for (int j = i + 1; j < albums.length; j++) {
+                if (albums[j] != null && albums[i] != null) {
+                    if (albums[j].getGenre().compareTo(albums[swapIndex].getGenre()) == -1) {
+                        swapIndex = j;
+                    }
+                }
+            }
+            Album tmpAlbum = albums[swapIndex];
+            albums[swapIndex] = albums[i];
+            albums[i] = tmpAlbum;
+        }
+        if (numAlbums != 0) {
+            System.out.println("*Album collection by genre.");
+            for (Album album : albums)
+                if (album != null)
+                    System.out.println(album);
+            System.out.println("*End of list");
+        } else {
+            System.out.println("The collection is empty!");
+        }
+    }
 }
-
