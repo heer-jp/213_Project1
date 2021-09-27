5c54feabf6e7731a30e67ced3fa455d91d5fdc96
Heer Patel <83523957+heer-jp@users.noreply.github.com>
2021-09-26 23:48:31 -0400

    Create RunProject1Copy.java

---
+++ src/RunProject1Copy.java
@@ -0,0 +1,15 @@
+package project1;
+/**
+ * Runs project1 by calling CollMan class run method. 
+ * @author Sai Maduri, Heer Patel
+ */
+public class RunProject1 {
+	/**
+	 * Calls CollMan run method which deals with input/output.
+	 * @param args user input
+	 */
+	public static void main(String [] args) {
+		new CollMan().run();
+	}
+
+}
